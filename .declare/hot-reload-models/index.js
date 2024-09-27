c795535a27ead766ba686aa7521b4e04a5d54470
Zach Blume <zachblume@gmail.com>
2024-09-27 17:49:17 -0400

    fixup docker and hot reloading sql, add comments

--- .declare/hot-reload-models/index.js
+++ .declare/hot-reload-models/index.js
@@ -1,18 +1,18 @@
 // Overwrites the db with models in models/**/*, this file executed every time a file in models/**/* changes
 const fs = require("fs");
-const {ClickHouseClient} = require("@clickhouse/client");
+const { ClickHouseClient } = require("@clickhouse/client");
 
 const executeSql = async (sql) => {
     const db = new ClickHouseClient({
         url: "http://clickhouse:8123",
         username: "default",
-        password: "default",
+        password: "password",
     });
 
     await db.query(sql);
 };
 
-module.exports = async function (db) {
+async function main(db) {
     // Recursively list all files in models/**/* along with metadata
     const files = await fs.promises.readdir("./models", {
         recursive: true,
@@ -20,20 +20,38 @@ module.exports = async function (db) {
     });
     const models = files
         .filter((file) => file.isFile())
+        // only files ending w .sql
+        .filter((file) => file.name == "definition.sql")
         .map((file) => {
-            const path = file.name;
-            const name = path.split("/").slice(1).join("/").replace(".js", "");
-            return { path, name };
+            const fullPath = `${file.parentPath}/${file.name}`;
+            const schema = file.parentPath.split("/")[1];
+            const table = file.parentPath.split("/")[2];
+            return {
+                fullPath,
+                schema,
+                table,
+            };
         });
     for (const model of models) {
         // Template the sql with CREATE OR REPLACE VIEW schema.table AS (...model.sql...);
-        const fileContents = await fs.promises.readFile(
-            `./models/${model.path}`,
-            { encoding: "utf8" }
+        const filePath = `./${model.fullPath}`;
+        console.log(
+            `Loading model ${model.schema}.${model.table} at ${model.fullPath}`
         );
-        const finalSql = `CREATE OR REPLACE VIEW ${model.name} AS (${fileContents})`;
+        const fileContents = await fs.promises.readFile(filePath, {
+            encoding: "utf8",
+        });
+        const finalSql = `CREATE OR REPLACE VIEW ${model.schema}.${model.table} AS (${fileContents})`;
 
         // Write it to db
-        executeSql(finalSql);
+        executeSql(finalSql).then(() => {
+            console.log(`Model ${model.schema}.${model.table} loaded`);
+        }).catch((err) => {
+            throw err;
+        })
     }
-};
+}
+
+main()
+    .then(() => console.log("Exiting"))
+    .catch(console.error);
--- Makefile
+++ Makefile
@@ -4,8 +4,6 @@ default: start open
 
 open:
 	@until nc -z localhost 5173 > /dev/null 2>&1; do sleep 0.2; done
-	@# Pause 200ms to wait for the server to start
-	@sleep 0.2
 	@echo "Opening dashboard server - http://localhost:5173"
 	@open http://localhost:5173
 
@@ -22,3 +20,6 @@ restart:
 clean:
 	docker-compose -f .declare/docker-compose.yml down
 	docker-compose -f .declare/docker-compose.yml rm -f
+
+dev:
+	docker-compose -f .declare/docker-compose.yml up --build
