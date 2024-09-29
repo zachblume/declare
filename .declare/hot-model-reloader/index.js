d9ba56c29c47ea9ca0235bdb0f9ae2f69e475a34
Zach Blume <zachblume@gmail.com>
2024-09-28 22:00:26 -0400

    rewrite with bun, needs dbouncing

--- .declare/hot-model-reloader/index.js
+++ .declare/hot-model-reloader/index.js
@@ -1,4 +1,6 @@
-// Overwrites the db with models in models/**/*, this file executed every time a file in models/**/* changes
+// Overwrites the db with models in models/**/*, this file executed every time
+// a file ending with definition.sql is changed
+import { watch, readFile } from "fs/promises";
 const fs = require("fs");
 const { createClient } = require("@clickhouse/client");
 
@@ -8,49 +10,70 @@ const db = createClient({
     password: process.env.CLICKHOUSE_PASSWORD ?? "",
 });
 
+// Watcher for file changes to models/**/**/definition.sql */
 async function main() {
-    // Recursively list all files in models/**/* along with metadata
+    reloadAllModels();
+    const watcher = watch("./", { recursive: true });
+    for await (const event of watcher) {
+        if (event.filename.endsWith("definition.sql")) {
+            reloadModel(event);
+        }
+    }
+}
+main();
+
+async function reloadAllModels() {
+    // Recursively list all files
     const files = await fs.promises.readdir("./models", {
         recursive: true,
         withFileTypes: true,
     });
     const models = files
-        .filter((file) => file.isFile())
-        // only files ending w .sql
-        .filter((file) => file.name == "definition.sql")
-        .map((file) => {
-            const fullPath = `${file.parentPath}/${file.name}`;
-            const database = file.parentPath.split("/")[1];
-            const table = file.parentPath.split("/")[2];
-            return {
-                fullPath,
-                database,
-                table,
-            };
-        });
+        .filter((file) => file?.name === "definition.sql")
+        .map((file) => ({ fullPath: `${file.parentPath}/${file.name}` }));
 
     // Load each model definition and apply it to db as a view, for quickness
-    for (const model of models) {
-        try {
-            const filePath = `./${model.fullPath}`;
-            const fileContents = await fs.promises.readFile(filePath, {
-                encoding: "utf8",
-            });
+    // Do it in parallel
+    await Promise.all(
+        models.map((model) =>
+            reloadModel({ filename: model.fullPath, shouldLog: false })
+        )
+    );
 
-            const createDatabaseSql = `CREATE DATABASE IF NOT EXISTS ${model.database};`;
-            const createViewSql = `CREATE OR REPLACE VIEW ${model.database}.${model.table} AS (${fileContents});`;
+    triggerEventLogWrite();
 
-            await db.command({ query: createDatabaseSql });
-            await db.command({ query: createViewSql });
-        } catch (err) {
-            console.error(
-                `Error while loading model ${model.database}.${model.table}:`,
-                err
-            );
-            throw err;
-        }
+    console.log("Models reloaded");
+}
+
+async function reloadModel({ shouldLog = false, ...event }) {
+    // Use bun api to reload a single model
+    const file = event.filename;
+    console.log(`Reloading model: ${file}`);
+    const matches = file.match(/models\/([^/]+)\/([^/]+)\/definition.sql/);
+    const database = matches[1];
+    const table = matches[2];
+
+    try {
+        const filePath = `./${file}`;
+        const fileContents = await readFile(filePath, { encoding: "utf8" });
+
+        const createDatabaseSql = `CREATE DATABASE IF NOT EXISTS ${database};`;
+        const createViewSql = `CREATE OR REPLACE VIEW ${database}.${table} AS (${fileContents});`;
+
+        await db.command({ query: createDatabaseSql });
+        await db.command({ query: createViewSql });
+    } catch (err) {
+        console.error(`Error while loading model ${database}.${table}:`, err);
+        throw err;
     }
 
+    if (shouldLog) {
+        console.log(`Model (re-)loaded: ${database}.${table}`);
+    }
+    triggerEventLogWrite();
+}
+
+async function triggerEventLogWrite() {
     try {
         // Write a log line to models/hot-model-reloader.log
         // Create the file if it doesn't exist
@@ -60,12 +83,4 @@ async function main() {
         console.error(`Error while writing log file`, err);
         throw err;
     }
-
-    console.log("Models reloaded");
 }
-
-main()
-    .then(() => {
-        /*do nothing*/
-    })
-    .catch(console.error);
