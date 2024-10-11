022d683c4bd5adbf5ff56b27b1de4127c0b50d06
Zach Blume <zachblume@gmail.com>
2024-10-11 00:52:43 -0400

    colors

--- __declare__/studio/src/components/GlobalLayout.jsx
+++ __declare__/studio/src/components/GlobalLayout.jsx
@@ -1,9 +1,10 @@
+import { Button, DropdownMenu } from "@radix-ui/themes";
 import { TabNav } from "@radix-ui/themes";
 import { Layers3Icon } from "lucide-react";
 
 export function GlobalLayout({ children }) {
     return (
-        <div className="global-layout">
+        <div className="flex flex-col h-screen">
             <Header />
             <main>{children}</main>
             <Footer />
@@ -20,6 +21,7 @@ function Header() {
                         className="text-neutral-500 mt-0.5"
                         size={17}
                         strokeWidth={1}
+                        color="red"
                     />
                     <span className="text-neutral-700">Workspace</span>
                     <span className="text-neutral-700 font-medium">/</span>
@@ -34,6 +36,54 @@ function Header() {
                 <TabNav.Link href="#">Connections</TabNav.Link>
                 <TabNav.Link href="#">Workflows</TabNav.Link>
                 <TabNav.Link href="#">Settings</TabNav.Link>
+                <div className="ml-auto mr-2">
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger>
+                            <Button variant="soft">
+                                Actions
+                                <DropdownMenu.TriggerIcon />
+                            </Button>
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <DropdownMenu.Item shortcut="⌘ E">
+                                Edit
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Item shortcut="⌘ D">
+                                Duplicate
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Item shortcut="⌘ N">
+                                Archive
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Sub>
+                                <DropdownMenu.SubTrigger>
+                                    More
+                                </DropdownMenu.SubTrigger>
+                                <DropdownMenu.SubContent>
+                                    <DropdownMenu.Item>
+                                        Move to project…
+                                    </DropdownMenu.Item>
+                                    <DropdownMenu.Item>
+                                        Move to folder…
+                                    </DropdownMenu.Item>
+                                    <DropdownMenu.Separator />
+                                    <DropdownMenu.Item>
+                                        Advanced options…
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.SubContent>
+                            </DropdownMenu.Sub>
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Item>Share</DropdownMenu.Item>
+                            <DropdownMenu.Item>
+                                Add to favorites
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Item shortcut="⌘ ⌫" color="red">
+                                Delete
+                            </DropdownMenu.Item>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </div>
             </TabNav.Root>
         </header>
     );
@@ -41,7 +91,7 @@ function Header() {
 
 function Footer() {
     return (
-        <footer>
+        <footer className="mt-auto py-2 bg-neutral-50 text-center">
             <p>Footer</p>
         </footer>
     );
--- __declare__/studio/src/main.jsx
+++ __declare__/studio/src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { Theme } from "@radix-ui/themes";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
-import { Theme, Button } from "@radix-ui/themes";
 
 const app = createRoot(document.getElementById("root"));
 
 app.render(
     <StrictMode>
         <BrowserRouter>
-            <Theme accentColor="gray" grayColor="olive">
+            <Theme accentColor="ruby" grayColor="olive">
                 <App />
             </Theme>
         </BrowserRouter>
