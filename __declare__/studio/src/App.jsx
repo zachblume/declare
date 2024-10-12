d164226d38573ef1b2ca34a0a4b0d7ead28d28de
Zach Blume <zachblume@gmail.com>
2024-10-12 01:41:13 -0400

    fix global layout

--- __declare__/studio/src/App.jsx
+++ __declare__/studio/src/App.jsx
@@ -1,25 +1,12 @@
 import routes from "~react-pages";
 import { useRoutes } from "react-router-dom";
 import { Suspense } from "react";
-import DatabasePage from "./pages/database";
-import ETLPage from "./pages/etl";
-import WorkflowsPage from "./pages/workflows";
-import MonitorsPage from "./pages/monitors";
-import LogsPage from "./pages/logs";
-import SettingsPage from "./pages/settings";
+import { GlobalLayout } from "components/GlobalLayout";
 
 export function App({ jwtToken }) {
     return (
         <Suspense fallback={<p>Loading...</p>}>
-            {useRoutes([
-                ...routes,
-                { path: "/database", element: <DatabasePage /> },
-                { path: "/etl", element: <ETLPage /> },
-                { path: "/workflows", element: <WorkflowsPage /> },
-                { path: "/monitors", element: <MonitorsPage /> },
-                { path: "/logs", element: <LogsPage /> },
-                { path: "/settings", element: <SettingsPage /> },
-            ])}
+            <GlobalLayout>{useRoutes([...routes])}</GlobalLayout>
         </Suspense>
     );
 }
