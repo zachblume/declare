b00cd67e7fcc910404bc5db9063d667931f42661
Zach Blume <zachblume@gmail.com>
2024-10-11 20:08:21 -0400

    Update __declare__/studio/src/App.jsx

--- __declare__/studio/src/App.jsx
+++ __declare__/studio/src/App.jsx
@@ -4,7 +4,6 @@ import { Suspense } from "react";
 import { GlobalLayout } from "./components/GlobalLayout";
 
 export function App({ jwtToken }) {
-
     return (
         <GlobalLayout>
             <Suspense fallback={<p>Loading...</p>}>
