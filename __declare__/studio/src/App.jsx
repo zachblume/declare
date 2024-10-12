13ea5cdf69159f595b8eef49b75199dddaf0dc43
Zach Blume <zachblume@gmail.com>
2024-10-11 20:08:10 -0400

    Update __declare__/studio/src/App.jsx

--- __declare__/studio/src/App.jsx
+++ __declare__/studio/src/App.jsx
@@ -4,15 +4,6 @@ import { Suspense } from "react";
 import { GlobalLayout } from "./components/GlobalLayout";
 
 export function App({ jwtToken }) {
-    const fetchDashboards = async () => {
-        const response = await fetch("/api/list-dashboards", {
-            headers: {
-                Authorization: `Bearer ${jwtToken}`,
-            },
-        });
-        const data = await response.json();
-        return data;
-    };
 
     return (
         <GlobalLayout>
